88093f4bf8d7c31e3911ac95721e425443e61379
nugrohomagfi <nugrohomagfi@gmail.com>
2026-02-01 18:54:58 +0700

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { HeroSection } from "@/components/hero-section"
 import { FeaturesSection } from "@/components/features-section"
 import { ProductsSection } from "@/components/products-section"
+import { Header } from "@/components/header"
+import { Footer } from "@/components/footer"
 
 export default function Home() {
   return (
@@ -9,9 +11,11 @@ export default function Home() {
       <div className="absolute inset-0 bg-grid-small opacity-100 dark:opacity-40 -z-10" />
       <div className="absolute inset-0 bg-gradient-to-b from-transparent via-background/50 to-background -z-10" />
 
+      <Header />
       <HeroSection />
       <FeaturesSection />
       <ProductsSection />
+      <Footer />
     </div>
   );
 }
